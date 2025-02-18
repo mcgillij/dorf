4c075caa778abdbb6ecd2be7a2b304de6245d677
mcgillij <mcgillivray.jason@gmail.com>
2025-02-17 20:01:29 -0400

    adding logger

--- client/daemon/whisper_worker.py
+++ client/daemon/whisper_worker.py
@@ -7,6 +7,14 @@ import asyncio  # Ensure you have this imported for running async code
 from dotenv import load_dotenv
 import aiohttp
 
+# Logging setup
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.FileHandler('whisper_worker.log'))
+logger.setLevel(logging.INFO)
+logger.Formatter('%(asctime)s - %(message)s')
+
 load_dotenv()
 # Configure Redis
 REDIS_HOST = os.getenv("REDIS_HOST", "")
@@ -31,13 +39,13 @@ class WhisperClient:
                         json_response = await response.json()
                         return json_response.get("text", "")
                     else:
-                        print(f"Error: {response.status} - {await response.text()}")
+                        logger.info(f"Error: {response.status} - {await response.text()}")
                         return ""
             except asyncio.TimeoutError:
-                print("Request timed out.")
+                logger.info("Request timed out.")
                 return "The whisper request timed out. Please try again later."
             except Exception as e:
-                print(f"Exception during API call: {e}")
+                logger.info(f"Exception during API call: {e}")
                 traceback.print_exc()
                 return "An error occurred while processing the request. Please try again later."
 
@@ -46,10 +54,10 @@ class WhisperWorker:
     def process_audio(self):
         """Process audio paths from the Redis queue."""
         # Connect to Redis
-        print(f"Connecting to Redis at {REDIS_HOST}:{REDIS_PORT}")
+        logger.info(f"Connecting to Redis at {REDIS_HOST}:{REDIS_PORT}")
         if not redis_client.ping():
             raise ConnectionError("Failed to connect to Redis.")
-        print("Connected to Redis successfully.")
+        logger.info("Connected to Redis successfully.")
         while True:
             try:
                 # Get a blocking pop from the queue (blocking until an item is available)
@@ -57,26 +65,26 @@ class WhisperWorker:
                     "whisper_queue", timeout=30
                 )  # Timeout of 30 seconds
                 if not path_data or len(path_data) < 2:
-                    # print("No data received from Redis queue.")
                     continue
                 key, raw_value = path_data  # Unpack the tuple correctly
-                print(f"Received key: {key}, Raw Value: {raw_value}")
+                logger.info(f"Received key: {key}, Raw Value: {raw_value}")
                 # Extract the audio_path and user_id from the Redis value
                 try:
                     path_info = json.loads(raw_value)
                     user_id = path_info.get("user_id")
                     audio_path = path_info.get("audio_path")
                     if not user_id or not audio_path:
-                        print("No valid user_id or audio_path in the received data.")
+                        logger.info("No valid user_id or audio_path in the received data.")
                         continue
-                    print(f"Processing {audio_path} for user_id: {user_id}...")
+                    logger.info(f"Processing {audio_path} for user_id: {user_id}...")
                     # Ensure WhisperClient.get_text is called within an event loop context
                     text_response = asyncio.run(self._get_text_from_audio(audio_path))
                     if text_response:
+                        logger.debug(f"Text response: {text_response}")
                         if bot_name_pattern.search(text_response):
                             # {"unique_id": 465152, "message": "427590626905948165: What's your favorite weapon?\n"}
                             # {"unique_id": "3a946d5e5d11350474220da38d878e38", "message": "427590626905948165:whats your favorite weapon"}
-                            print(f"Text response: {text_response}")
+                            logger.info(f"replying_to: {text_response}")
                             unique_id = randint(
                                 100000, 999999
                             )  # Generate a random unique ID
@@ -89,22 +97,22 @@ class WhisperWorker:
                                     }
                                 ),
                             )
-                            print(f"Pushed response to Redis queue.")
+                            logger.info(f"Pushed response to Redis queue.")
                             # now we can remove the audio file
                         else:
-                            print(
+                            logger.info(
                                 f"No bot name found in text response: {text_response}"
                             )
                         os.remove(audio_path)
 
                     else:
-                        print("No text response received.")
+                        logger.info("No text response received.")
                 except json.JSONDecodeError as e:
-                    print(f"Failed to decode JSON from Redis: {e}")
+                    logger.info(f"Failed to decode JSON from Redis: {e}")
             except redis.exceptions.TimeoutError:
-                print("Redis queue timeout occurred.")
+                logger.info("Redis queue timeout occurred.")
             except Exception as e:
-                print(f"Exception during processing: {e}")
+                logger.info(f"Exception during processing: {e}")
 
     async def _get_text_from_audio(self, audio_path):
         """Get text from the given audio path using WhisperClient."""
@@ -122,3 +130,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
